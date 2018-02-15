4dfdf4ffb3ebe4e30a123971a149e3f52a37ff32
Anel <anel_taric96@live.com>
2018-02-15 16:52:03 +0100

    Anel commit

---
+++ Can You Open It/Assets/Scripts/Click.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+    public UnityEngine.UI.Text gpc;
+    public UnityEngine.UI.Text goldDisplay;
+    public float gold = 0.00f;
+    public int goldperclick = 1;
+
+    void Update()
+    {
+        goldDisplay.text = "Gold: " + CurrentiesConverter.Instance.GetCurrencyIntoString(gold, false, false);
+        gpc.text = CurrentiesConverter.Instance.GetCurrencyIntoString(goldperclick, false, true) + " gold/click";
+    }
+
+    public void Clicked()
+    {
+        gold += goldperclick;
+
+    }
+}
---
+++ Can You Open It/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UpgradeManager : MonoBehaviour {
+
+    public Click click;
+    public UnityEngine.UI.Text itemInfo;
+    public float cost;
+    public int count = 0;
+    public int clickPower;
+    public string itemName;
+    public Color standard;
+    public Color affordable;
+    private float baseCost;
+    private Slider _slider;
+
+    private void Start()
+    {
+        baseCost = cost;
+       _slider = GetComponentInChildren<Slider> ();
+    }
+
+
+    void Update()
+    {
+        itemInfo.text = itemName + "\nCost: " + cost + "\nPower: +" + clickPower;
+
+        /*if (click.gold >= cost)
+        {
+           gettComponent<Image> ().color = affordable;
+        }   else
+        {
+            GetComponent<Image> ().color = standard;
+        }*/
+        _slider.value = click.gold / cost * 100;
+        
+    }
+
+    public void PurchasedUpgrade()
+    {
+        if (click.gold >= cost)
+        {
+            click.gold -= cost;
+            count += 1;
+            click.goldperclick += clickPower;
+            cost = baseCost * Mathf.Pow(1.15f, count);
+        }
+    }
+}
