758e937a667cd1c4fbf329ad412886bdfa6c9272
Dejan Misic <dmisic@gmail.com>
2018-02-17 01:02:38 +0100

    Committing additional projects
    Commiting two old projects in order to extend resources for building CanYouOpenIt

---
+++ Loading Screen and Start Menu/Assets/Scripts/ImageDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ImageDisplay : MonoBehaviour {
+	public Texture imageToDisplay;
+	public float timeToDisplayImage;
+	public int nextLevelToLoad;
+
+	private float timeForNextLevel;
+
+	public void Start() {
+		timeForNextLevel = Time.time + timeToDisplayImage;
+	}
+
+	public void OnGUI() {
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), imageToDisplay);
+		if (Time.time >= timeForNextLevel) {
+			SceneManager.LoadScene(nextLevelToLoad);
+		}
+	}
+}
