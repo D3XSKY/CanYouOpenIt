9bece18ca079c4bcda6774f57229e19238a4ca11
Dejan Misic <dmisic@gmail.com>
2018-02-19 00:04:39 +0100

    Activating Unity Services
    Committing changes where unity services should be activated

---
+++ Can You Open It/Assets/Scripts/AdManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Unity Ads initialized: " + Advertisement.isInitialized);
+        Debug.Log("Unity Ads is supported: " + Advertisement.isSupported);
+        Debug.Log("Unity Ads test mode enabled: " + Advertisement.testMode);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ShowVideoAd(Action<ShowResult> adCallBackAction = null, string zone = "")
+    {
+
+        StartCoroutine(WaitForAdEditor());
+
+        if (string.IsNullOrEmpty(zone))
+        {
+            zone = null;
+        }
+
+        var options = new ShowOptions();
+
+        if (adCallBackAction == null)
+        {
+            options.resultCallback = DefaultAdCallBackHandler;
+        }
+        else
+        {
+            options.resultCallback = adCallBackAction;
+        }
+
+        if (Advertisement.IsReady(zone))
+        {
+            Debug.Log("Showing ad for zone: " + zone);
+            Advertisement.Show(zone, options);
+        }
+        else
+        {
+            Debug.LogWarning("Ad was not ready. Zone: " + zone);
+        }
+    }
+    private void DefaultAdCallBackHandler(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Time.timeScale = 1f;
+                break;
+
+            case ShowResult.Failed:
+                Time.timeScale = 1f;
+                break;
+
+            case ShowResult.Skipped:
+                Time.timeScale = 1f;
+                break;
+        }
+    }
+    public void ShowStandardVideoAd()
+    {
+        ShowVideoAd();
+    }
+}
