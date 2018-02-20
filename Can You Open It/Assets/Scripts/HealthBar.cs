61dd711354bbf0fb8dfcc4fea2fba16b796584ad
Dejan Misic <dmisic@gmail.com>
2018-02-20 23:41:48 +0100

    Adding health bar
    Added HealthBar.cs
    Connected it with graphics so health will be reduced on click

---
+++ Can You Open It/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+    public Image bar;
+    public float maxHealth = 100f;
+    public float currentHealth = 0f;
+    // Use this for initialization
+    void Start () {
+        currentHealth = maxHealth;
+        //InvokeRepeating("DecreaseHealth", 0f, 2f);
+	}
+	public void DecreaseHealth()
+    {
+        currentHealth -= 1f;
+        float calculateHealth = currentHealth / maxHealth;
+        SetHealth(calculateHealth);
+    }
+    void SetHealth(float myHealth) {
+
+        bar.fillAmount = myHealth;
+    }
+}
